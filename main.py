8ce7178cc46714f9aafd0bab86439377b8bad909
BB20556 <bouchraberrissoul@gmail.com>
2026-05-13 15:18:32 -0400

    first_commit_main_v1_with_addition

---
+++ main.py
@@ -0,0 +1,11 @@
+import math
+
+print( math.sqrt(34) )
+
+
+def somme_chiffres(n):
+    if n < 10:
+        return n
+    return n % 10 + somme_chiffres(n // 10)
+print(somme_chiffres(1234))
+print(somme_chiffres(123))
